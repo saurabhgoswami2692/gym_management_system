d86fcc0264098ad9f3dc56d1227c451e2cb90ff4
saurabhsemi26 <76123973+saurabhsemi26@users.noreply.github.com>
2024-12-04 22:52:36 +0530

    add the update, getAllMembers and delete functions in member controller and add routes

--- server/controller/memberController.js
+++ server/controller/memberController.js
@@ -18,3 +18,52 @@ export const createMember = async(req, res) => {
         res.status(500).json({errorMessage:error.message});
     }
 }
+
+export const getAllMembers = async(req,res) => {
+    try {
+        const members = await memberModel.find();
+        const totalMembers = await memberModel.countDocuments();
+        if(!members || members.length === 0 ) {
+            return res.status(400).json({message:"Data not found."});
+        }
+
+        res.status(200).json({
+            members:members,
+            totalMembers: totalMembers
+        });
+
+
+    } catch (error) {
+        res.status(500).json({errorMessage:error.message});
+    }
+}
+
+export const updateMember = async(req, res) => {
+
+    try {
+        const id = req.params.id;
+        const userExist  = await memberModel.findById(id);
+        if(!userExist){
+            return res.status(400).json({message:"Data not found."});
+        }
+
+        const updatedData = req.body;
+        await memberModel.findByIdAndUpdate(id,updatedData);
+        
+        res.status(200).json({message:"Member updated successfully."});
+
+    } catch (error) {
+        res.status(500).json({errorMessage: error.message});
+    }
+
+}
+
+export const deleteMember = async(req, res) => {
+    try {
+        const id = req.params.id;
+        await memberModel.findByIdAndDelete(id);
+        res.status(200).json({message:"Member deleted successfully."});
+    } catch (error) {
+        res.status(500).json({errorMessage:error.message});
+    }
+}
--- server/routes/Route.js
+++ server/routes/Route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createMember } from "../controller/memberController.js";
+import { createMember, deleteMember, getAllMembers, updateMember } from "../controller/memberController.js";
 
 const route = express.Router();
 
-// added routes
 route.post('/create-member', createMember);
-
+route.get('/members',getAllMembers);
+route.put('/update-member/:id',updateMember);
+route.post('/delete-member/:id',deleteMember);
 export default route;
