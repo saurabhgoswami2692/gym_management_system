86aa1777fb5846800937acfc486ccd4bda7e9e5c
saurabhsemi26 <76123973+saurabhsemi26@users.noreply.github.com>
2024-12-04 22:10:46 +0530

    Add comment

--- server/routes/Route.js
+++ server/routes/Route.js
@@ -3,6 +3,7 @@ import { createMember } from "../controller/memberController.js";
 
 const route = express.Router();
 
+// added routes
 route.post('/create-member', createMember);
 
 export default route;
